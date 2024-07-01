2ca27934bd219494c09c15fec87306301d6124e2
YeoungKwon <dudrnjs25602daum.net>
2024-07-02 04:29:47 +0900

    test 3

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -10,7 +10,7 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 
 
-		System.out.println(" 하 드디어 됐네 진짜;");
+		System.out.println(" 하 드디어 됐네 진짜;/n 진짜 깃허브 연동 이제는 된거지? 맥 터미널 쓰는거 왤케 어렵냐 리눅스도 공부해야겠다");
 	}
 
 }
