89d6c26d9200a7d54d3fae5465311df90efbe6d4
YeoungKwon <dudrnjs25602daum.net>
2024-07-03 02:43:15 +0900

    2024 07 03

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,99 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request); // 컨트롤 + t 리팩토링
+        printHeaders(request);
+        printHeaderUtils(request);
+    }
+
+    /*start header*/
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " + request.getQueryString()); // 없으면 null
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용유무
+        System.out.println();
+        System.out.println("--- REQUEST-LINE - end ---");
+    }
+
+    /*header 모든정보*/
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println(" --------- Headers - start ------- ");
+
+        /*1번쨰 방법 */
+        /*Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName+ ":" + headerName);
+        }*/
+
+
+        /*2번째 방법 람다식 간결. */
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+
+
+        request.getHeader("host"); /*header 하나만 조회하고싶을때.*/
+
+
+        System.out.println(" --------- Headers - end ------- ");
+
+    }
+
+
+    private void printHeaderUtils(HttpServletRequest request) {
+
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " +
+                request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+                request.getServerPort()); //Host 헤더
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocales() = " + request.getLocales());
+        System.out.println();
+
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " +
+                request.getContentType());
+
+        System.out.println("request.getContentLength() = " +
+                request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " +
+                request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println(); // post man 사용 세팅값 method - post url http://localhost:8080/request-header raw text/plan hello ->
+
+    }
+
+}
+
